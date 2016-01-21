54f7de2139110015600be8c29b3e13c816be4d56
Liang Ding <dl88250@gmail.com>
2016-01-21 11:40:13 +0800

    文案

--- src/main/java/org/b3log/symphony/util/Mails.java
+++ src/main/java/org/b3log/symphony/util/Mails.java
@@ -72,7 +72,7 @@ public final class Mails {
             formData.put("api_user", API_USER);
             formData.put("api_key", API_KEY);
             formData.put("from", "admin@" + Latkes.getServerHost());
-            formData.put("fromname", "黑客派");
+            formData.put("fromname", "Sym");
             formData.put("subject", subject);
             formData.put("template_invoke_name", templateName);
 
